4cebe63f686532255c74102efd0785b10c7b8c9c
Roman Ortega Mondaca <romtega@gmail.com>
2017-06-26 23:24:45 -0500

    Customized the Work resources name

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,6 @@
 class WorksController < ApplicationController
+
+def index
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :works
-  
+  resources :work, controller: 'works'
+
   get 'jazz', to: 'pages#blog'
   get 'profile', to: 'pages#profile'
   get 'contact', to: 'pages#contact' 
