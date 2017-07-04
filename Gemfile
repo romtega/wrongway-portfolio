70a86a3cf78319fedba0726e6b0422d65f10a426
Roman Ortega Mondaca <romtega@gmail.com>
2017-07-03 19:06:03 -0500

    Implemented font awesome and created the contact view

--- Gemfile
+++ Gemfile
@@ -31,4 +31,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'jquery-rails'
 gem 'popper_js', '~> 1.9.9'
+gem 'font-awesome-rails', '~> 4.7', '>= 4.7.0.2'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,44 @@ module ApplicationHelper
     "@ #{Time.now.year} by <a href=""><b>Roman Ortega Mondaca</b></a>, All Rights Reserved.".html_safe
   end 
 
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: profile_path,
+        title: 'Profile'
+      },
+      {
+        url: work_index_path,
+        title: 'Work'
+      },
+      {
+        url: jazz_path,
+        title: 'Jazz'
+      },   
+      {
+        url: contact_path,
+        title: 'Contact'
+      },                  
+    ]
+  end
+
+  def nav_helper
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<a href='#{item[:url]}' class='nav-link #{active? item[:url]}'>#{item[:title]}</a>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
 end
 
